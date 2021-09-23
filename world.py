468734acbda1f54f2e2256297b37a1e21e0acfe5
Jarek Majka <vessekheal@gmail.com>
2021-09-23 22:25:13 +0200

    World class shortly described

--- world.py
+++ world.py
@@ -1,3 +1,12 @@
 class World:
+    # klasa zarządzająca rozgrywką i organizmami na planszy
     def __init__(self):
+        pass
+
+    def make_a_turn(self):
+        # Sprawi, że organizmy wykonają swój ruch zgodnie z założeniami.
+        pass
+
+    def create_world(self):
+        # Stworzy świat o podanych przez użytkownika wymiarach
         pass
