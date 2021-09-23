52348f6b33ea8275630c65bfbe26844f41829e32
Jarek Majka <vessekheal@gmail.com>
2021-09-23 22:51:01 +0200

    create_world method updated

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+from world import World
+
+
 if __name__ == '__main__':
-    print('Hello, World!')
+    pass
--- world.py
+++ world.py
@@ -1,12 +1,13 @@
 class World:
     # klasa zarządzająca rozgrywką i organizmami na planszy
     def __init__(self):
-        pass
+        self.n_axis = None
+        self.m_axis = None
 
     def make_a_turn(self):
         # Sprawi, że organizmy wykonają swój ruch zgodnie z założeniami.
         pass
 
     def create_world(self):
-        # Stworzy świat o podanych przez użytkownika wymiarach
-        pass
+        self.n_axis = int(input('Podaj szerokość świata: '))
+        self.m_axis = int(input('Podaj długość świata: '))
