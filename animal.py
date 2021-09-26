6a980bcfacc6faca7debff2b6fb714d964445a74
Jarek Majka <vessekheal@gmail.com>
2021-09-26 22:17:40 +0200

    Movement and movement sanity check methods moved to base class

--- animal.py
+++ animal.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from organism import Organism
 
 
@@ -13,4 +15,33 @@ class Animal(Organism):
         # zwierzęta w ramach metody kolizja jeżeli napotkają zwierzę tego samego gatunku nie zmieniają swego położenia
         # a obok nich pojawia się trzecie zwierzę tego samego gatunku.
         pass
-    
+
+    def movement_sanity_check(self, new_position: Tuple, world_reference: object) -> Tuple:
+        """
+        Checks if Char is not at the edge of the map and can move in the desired direction.
+        :return: bool value
+        """
+        if new_position[0] <= 0 \
+                or new_position[1] <= 0 \
+                or new_position[0] > world_reference.n_axis \
+                or new_position[1] > world_reference.m_axis:
+            print('Nie można iść w tym kierunku, pozcyja nie zmieniona.')
+        else:
+            self.position = new_position
+
+    def walk_in_desired_direction(self, direction: str, world_reference: object) -> Tuple:
+        """
+        :param world_reference: world reference object for world dimensions check
+        :param direction: str value of the direction
+        :return: updated position
+        """
+        if direction == 'n':
+            new_position = (self.position[0] + 1, self.position[1])
+        elif direction == 's':
+            new_position = (self.position[0] - 1, self.position[1])
+        elif direction == 'e':
+            new_position = (self.position[0], self.position[1] + 1)
+        elif direction == 'w':
+            new_position = (self.position[0], self.position[1] - 1)
+        self.movement_sanity_check(new_position, world_reference)
+        print(self.position)
--- human.py
+++ human.py
@@ -15,7 +15,7 @@ class Human(Animal):
         # Człowiek może przemieścić się jedynie na sąsiednie pole
         # ale kierunek jego ruchu definiowany jest przez uzytkownika poprzez wciśnięcie odpowiedniego klawisza
         action = self.movement_menu()
-        self.walk_in_desired_direction(action)
+        self.walk_in_desired_direction(action, self.world_reference)
 
     def collision(self):
         # Człowiek będzie posiadał specjalną umiejętność, którą można aktywować
@@ -36,32 +36,3 @@ class Human(Animal):
         elif menu_choice == '2':
             # TODO implement special skill method
             pass
-
-    def movement_sanity_check(self, new_position: Tuple) -> Tuple:
-        """
-        Checks if Char is not at the edge of the map and can move in the desired direction.
-        :return: bool value
-        """
-        if new_position[0] <= 0 \
-                or new_position[1] <= 0 \
-                or new_position[0] > self.world_reference.n_axis \
-                or new_position[1] > self.world_reference.m_axis:
-            print('Nie można iść w tym kierunku, pozcyja nie zmieniona.')
-        else:
-            self.position = new_position
-
-    def walk_in_desired_direction(self, direction: str) -> Tuple:
-        """
-        :param direction: str value of the direction
-        :return: updated position
-        """
-        if direction == 'n':
-            new_position = (self.position[0] + 1, self.position[1])
-        elif direction == 's':
-            new_position = (self.position[0] - 1, self.position[1])
-        elif direction == 'e':
-            new_position = (self.position[0], self.position[1] + 1)
-        elif direction == 'w':
-            new_position = (self.position[0], self.position[1] - 1)
-        self.movement_sanity_check(new_position)
-        print(self.position)
