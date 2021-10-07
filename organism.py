fd67be31702bb1aabcfe49606c0f2abdb59b54f8
Jarek Majka <vessekheal@gmail.com>
2021-10-07 22:22:54 +0200

    Position unchanged message updated

--- organism.py
+++ organism.py
@@ -16,29 +16,10 @@ class Organism:
                 or new_position[1] <= 0 \
                 or new_position[0] > world_reference.n_axis \
                 or new_position[1] > world_reference.m_axis:
-            print('Nie można iść w tym kierunku, pozycja nie zmieniona.')
+            print(f'{self} tried to make an invalid move. Position remains unchanged')
         else:
             print(f'{self} moved to {new_position}')
             self.position = new_position
-
-    # def move_in_desired_direction(self, world_reference: object, direction=None) -> None:
-    #     """
-    #     :param world_reference: world reference object for world dimensions check
-    #     :param direction: str value of the direction
-    #     :return: updated position
-    #     """
-    #     if not direction:
-    #         direction = choice('news')
-    #
-    #     if direction == 'n':
-    #         new_position = (self.position[0] + 1, self.position[1])
-    #     elif direction == 's':
-    #         new_position = (self.position[0] - 1, self.position[1])
-    #     elif direction == 'e':
-    #         new_position = (self.position[0], self.position[1] + 1)
-    #     else:
-    #         new_position = (self.position[0], self.position[1] - 1)
-    #     self.movement_sanity_check(new_position, world_reference)
 
     def starting_position(self, world_reference: object) -> Tuple:
         return randrange(world_reference.n_axis) + 1, randrange(world_reference.m_axis) + 1
