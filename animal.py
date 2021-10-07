b5590d4960fdfa83d20dbb639d7379c111f33df1
Jarek Majka <vessekheal@gmail.com>
2021-10-07 21:46:52 +0200

    Minor changes in animal and world scripts

--- animal.py
+++ animal.py
@@ -2,4 +2,8 @@ from organism import Organism
 
 
 class Animal(Organism):
-    pass
+    def multiplication(self, free_fields: list):
+        def check_organism_type():
+            print(type(self))
+
+        check_organism_type()
